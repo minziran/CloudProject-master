0a8d9eacf9f6f0f88a77ee91ae7102e88f2fa97f
minziran <minziran19950716@163.com>
2017-12-10 20:06:04 -0600

    Init

---
+++ Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/CharacterController/vThirdPersonController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Invector.CharacterController
+{
+    public class vThirdPersonController : vThirdPersonAnimator
+    {
+        public GameObject beam;
+        public Transform origin;
+        private Coroutine shootCoroutine;
+        
+        protected virtual void Start()
+        {
+#if !UNITY_EDITOR
+                Cursor.visible = false;
+#endif
+        }
+
+        public virtual void Sprint(bool value)
+        {                                   
+            isSprinting = value;            
+        }
+
+        public virtual void Strafe()
+        {
+            if (locomotionType == LocomotionType.OnlyFree) return;
+            isStrafing = !isStrafing;
+        }
+
+        public virtual void Jump()
+        {
+            // conditions to do this action
+            bool jumpConditions = isGrounded && !isJumping;
+            // return if jumpCondigions is false
+            if (!jumpConditions) return;
+            // trigger jump behaviour
+            jumpCounter = jumpTimer;            
+            isJumping = true;
+            // trigger jump animations            
+            if (_rigidbody.velocity.magnitude < 1)
+                animator.CrossFadeInFixedTime("Jump", 0.1f);
+            else
+                animator.CrossFadeInFixedTime("JumpMove", 0.2f);
+            
+            cancelShoot();
+        }
+
+		public virtual void Attack()
+		{
+		    if (isJumping || !isGrounded)
+            {
+                return;
+            }
+			Debug.Log ("Attack");
+			photonView.RPC ("RPCAttack", PhotonTargets.All);
+		}
+
+		[PunRPC]
+		public virtual void RPCAttack() 
+        {
+            if (animator == null)
+                animator = GetComponent<Animator> ();    
+            animator.CrossFadeInFixedTime("spell", 0.2f);
+            
+            cancelShoot();
+            shootCoroutine = StartCoroutine(ShootBeam());
+        }
+        
+        IEnumerator ShootBeam()
+        {
+            yield return new WaitForSeconds(1.5f);
+            beam.SetActive(true);
+            yield return new WaitForSeconds(.75f);
+            beam.SetActive(false);
+        }
+        
+        private void cancelShoot()
+        {
+            if (shootCoroutine != null)
+            {
+                StopCoroutine(shootCoroutine);
+            }
+            beam.SetActive(false);
+        }
+
+        public virtual void RotateWithAnotherTransform(Transform referenceTransform)
+        {
+            var newRotation = new Vector3(transform.eulerAngles.x, referenceTransform.eulerAngles.y, transform.eulerAngles.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(newRotation), strafeRotationSpeed * Time.fixedDeltaTime);
+            targetRotation = transform.rotation;
+        }
+    }
+}
---
+++ Assets/zzzzProject/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : MonoBehaviour {    
+
+    public GameObject lobbyCamera;
+    public GameObject light;
+    public GameObject player;
+
+    [SerializeField] public Transform[] spawnPoint;
+
+	public InputField new_room;
+	public InputField nameField;
+
+
+
+    private const string GAME_VERSION = "0.1";
+	private bool isConnecting;
+
+    void Awake () {
+        Debug.Log ("Connect to network");
+        PhotonNetwork.autoJoinLobby = false;
+		PhotonNetwork.automaticallySyncScene = true;
+        //PhotonNetwork.ConnectUsingSettings (GAME_VERSION);
+    }
+
+	void Start(){
+		Connect();
+		SetPlayerName();
+	}
+
+	public void Connect(){
+	
+		isConnecting = true;
+		if (PhotonNetwork.connected) {
+
+			Debug.Log ("Connected");
+
+		} else {
+			PhotonNetwork.ConnectUsingSettings (GAME_VERSION);
+		
+		}
+	}
+
+
+	public void CreateARoom(){
+		if (PhotonNetwork.connected) {
+			if (PhotonNetwork.CreateRoom (new_room.text, new RoomOptions{ MaxPlayers = 4 }, null)) {
+				Debug.Log ("Already create a room");
+				PhotonNetwork.LoadLevel ("ROOM");
+			
+			}
+		}
+	}
+
+
+
+	public void SetPlayerName(){
+		if (string.IsNullOrEmpty (nameField.text)) {
+			if (PlayerPrefs.HasKey ("PlayerName")) {
+				
+				nameField.text = PlayerPrefs.GetString ("PkayerName");
+			}
+		}
+		PhotonNetwork.playerName = nameField.text;
+		PlayerPrefs.SetString ("PlayerName", nameField.text);
+	
+	}
+
+	public void GetStart(){
+	
+		PhotonNetwork.LoadLevel("Main");
+	
+	}
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        //PhotonNetwork.JoinOrCreateRoom("Room0", null, null);
+    }
+
+	public virtual void OnCreatedRoom(){
+        Debug.Log("Created room");
+		lobbyCamera.SetActive (false);
+		PhotonNetwork.JoinOrCreateRoom (new_room.text, null, TypedLobby.Default);
+
+    }
+
+
+	public virtual void OnJoinedRoom() {
+        Debug.Log ("Joined room");
+		PhotonNetwork.automaticallySyncScene = true;
+        lobbyCamera.SetActive (false);
+		light.SetActive (false);
+        PhotonNetwork.Instantiate (player.name, spawnPoint[0].position, spawnPoint[0].rotation, 0);
+    }
+
+
+    public virtual void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        Debug.Log("Player connected");
+    }
+
+    public virtual void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+    {
+        Debug.Log("Player disconnected");
+    }
+
+
+    public virtual void OnConnectionFail(DisconnectCause cause)
+    {
+        Debug.Log("Connection failed to the Photon network");
+    }
+//
+    public virtual void OnDisconnectedFromPhoton()
+    {
+        Debug.Log("We got disconnected form the Photon network");
+    }
+}
+
